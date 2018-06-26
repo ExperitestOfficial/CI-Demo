44daed3a17a4d39724b373b6b43d0f82aca0b3c1
Mirron Rozanov <mirron@experitest.com>
2018-06-26 17:06:19 +0300

    Revert "changed build number"
    This reverts commit 5004f03

--- src/main/java/com/experitest/auto/TestBase.java
+++ src/main/java/com/experitest/auto/TestBase.java
@@ -1,8 +1,15 @@
 package com.experitest.auto;
 
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class TestBase {
 
@@ -12,9 +19,11 @@ public class TestBase {
 	@BeforeSuite
 	public void setUp(ITestContext testContext) throws Exception {
 
+
+
 		dc = new DesiredCapabilities();
 		dc.setCapability("stream", "CI-Run");
-		dc.setCapability("build.number", 100);
+		dc.setCapability("build.number", System.getenv("BUILD_NUMBER"));
 
 		dc.setCapability("accessKey", System.getenv("ACCESS_KEY"));
 		dc.setCapability("testName", "Sanity - Experitest.com");
