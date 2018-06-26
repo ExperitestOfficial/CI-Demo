5004f03ea266fc4b79232b4691751a0918c7c942
Sheliz <Sheli@experitest.com>
2018-06-26 16:58:01 +0300

    changed build number

--- src/main/java/com/experitest/auto/TestBase.java
+++ src/main/java/com/experitest/auto/TestBase.java
@@ -1,15 +1,8 @@
 package com.experitest.auto;
 
-import java.net.URL;
-
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 public class TestBase {
 
@@ -19,11 +12,9 @@ public class TestBase {
 	@BeforeSuite
 	public void setUp(ITestContext testContext) throws Exception {
 
-
-
 		dc = new DesiredCapabilities();
 		dc.setCapability("stream", "CI-Run");
-		dc.setCapability("build.number", System.getenv("BUILD_NUMBER"));
+		dc.setCapability("build.number", 100);
 
 		dc.setCapability("accessKey", System.getenv("ACCESS_KEY"));
 		dc.setCapability("testName", "Sanity - Experitest.com");
